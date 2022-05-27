2dd7d4bc71e0fd5a75e2bf4bb01262e7c8da45c2
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-26 21:06:30 -0300

    Criando template da página

---
+++ sistema/src/components/pageTemplate.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container">
+  <div class="row position-center--class">
+    <b-aspect aspect="4:3" class="col-8 central-element">
+    <div>
+    </div>
+    </b-aspect>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name:'pageTemplate.vue'
+}
+</script>
+
+<style lang="scss" scoped>
+@import  '../../public/variaveis.scss';
+
+div.position-center--class{
+display:flex;
+height: 100vh;
+justify-content: center;
+align-items: center;
+overflow: hidden;
+}
+div.central-element{
+  border-radius: 30px 30px;
+  background-color:$secondary-color-0;
+  display:flex;
+box-shadow: 0px 5px 20px -10px $secondary-color-7;
+
+
+}
+
+</style>
--- sistema/src/views/PersonalDataPage.vue
+++ sistema/src/views/PersonalDataPage.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
-    Olá
-  </div>
+  <pageTemplate>
+   aaa
+  </pageTemplate>
+
 </template>
 
 <script>
+import pageTemplate from "../components/pageTemplate.vue";
 
 export default {
   name:'PersonalDataPage',
-  components:{}
+  components:{pageTemplate}
 }
 </script>
 
 <style lang="scss" scoped>
 @import  '../../public/variaveis.scss';
-div{
-  color:$primary-color;
-}
 </style>
