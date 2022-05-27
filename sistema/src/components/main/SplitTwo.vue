3b22e9dbeca956778f04c1ad9ad240301d71dcae
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 20:36:48 -0300

    Criando componente de divisão de elementos

---
+++ sistema/src/components/main/SplitTwo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="split">
+  <slot class="flex1" name="one"></slot>
+  <slot class="flex1" name="two"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.split{
+display:flex;
+flex-wrap: nowrap;
+width: 100%;
+}
+</style>
