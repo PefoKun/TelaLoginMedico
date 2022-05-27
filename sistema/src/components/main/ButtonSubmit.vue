e4be30139b114d8b8552d01b2b2db55a9f64d008
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 20:36:29 -0300

    Criando Componente Botão de Enviar

---
+++ sistema/src/components/main/ButtonSubmit.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-button class="button-submit" type="submit" variant="primary">Próximo</b-button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import  '../../assets/variaveis.scss';
+.button-submit{
+  background-color:$primary-color-0;
+  text-transform: uppercase;
+  border-radius: 15px;
+}
+</style>
