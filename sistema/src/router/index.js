072275b61ac3ff6b66ee6692fe60f55e963bf487
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 21:12:42 -0300

    Adição de Rota para a segunda página (AtendimentoPage)

--- sistema/src/router/index.js
+++ sistema/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import PersonalDataPage from '../views/PersonalDataPage.vue'
+import AtendimentoDataPage from '../views/AtendimentoDataPage.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +10,11 @@ const routes = [
     name: 'home',
     component: PersonalDataPage
   },
+  {
+    path:'/sobre-o-atendimento',
+    name: 'paginaAtendimento',
+    component: AtendimentoDataPage
+  }
 ]
 
 const router = new VueRouter({
