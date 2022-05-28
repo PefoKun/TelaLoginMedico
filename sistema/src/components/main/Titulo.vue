670ea1ca885b7747659870ee14aff58487c49443
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 21:12:27 -0300

    Atualização na grossura do Titulo

--- sistema/src/components/main/Titulo.vue
+++ sistema/src/components/main/Titulo.vue
@@ -12,7 +12,7 @@ name:'TituloPrincipal'
 @import  '../../assets/variaveis.scss';
 .title{
   font-size: 40px;
-  font-weight: 700;
+  font-weight: 800;
   color:$primary-color-0;
   margin-bottom: 30px;
 }
