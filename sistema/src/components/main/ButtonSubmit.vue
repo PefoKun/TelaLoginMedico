7fc0079f13704416a11321ff1f41367803eba019
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 21:12:10 -0300

    Atualização no componente botão de enviar

--- sistema/src/components/main/ButtonSubmit.vue
+++ sistema/src/components/main/ButtonSubmit.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-button class="button-submit" type="submit" variant="primary">Próximo</b-button>
+  <b-button class="button-submit" type="submit">Próximo</b-button>
 </template>
 
 <script>
@@ -14,5 +14,14 @@ export default {
   background-color:$primary-color-0;
   text-transform: uppercase;
   border-radius: 15px;
+  width: 95%;
+  &:active{
+    border:1px solid $secondary-color-7;
+  }
+  &:hover{
+    background-color: #33266c;
+  }
 }
+
+
 </style>
