492a130c573d6da8b81804337b9469756675be20
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-27 21:12:49 -0300

    Create AtendimentoDataPage.vue

---
+++ sistema/src/views/AtendimentoDataPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <PageTemplate class='position-relative'>
+  <div class="container m-5">
+    <div class="row ml-3">
+      <div class="col title">
+      <Titulo>Sobre o atendimento</Titulo>
+      <SubTitulo>Detalhes do atendimento</SubTitulo>
+
+      <FormularioPersonalDataPage/>
+      
+      </div>
+    </div>
+  </div>
+  </PageTemplate>
+</template>
+
+<script>
+import PageTemplate from "../components/PageTemplate.vue";
+import Titulo from "../components/main/Titulo.vue"
+import SubTitulo from "@/components/main/SubTitulo.vue";
+import FormularioPersonalDataPage from "../components/formularioPersonalDataPage.vue";
+export default {
+  name:'AtendimentoDataPage',
+  components:{ PageTemplate, Titulo, SubTitulo, FormularioPersonalDataPage }
+}
+</script>
+
+<style lang="scss" scoped>
+@import  '../assets/variaveis.scss';
+
+
+</style>
