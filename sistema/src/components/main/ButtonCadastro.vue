8d73c66e43da6b7aeb9a96dbfc2455a937f38624
Paulo Emmanuel <pauloemmanuelferreira@gmail.com>
2022-05-28 20:14:19 -0300

    Botão de Cadastro

---
+++ sistema/src/components/main/ButtonCadastro.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-button class="button-submit" type="submit">Próximo</b-button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import  '../../assets/variaveis.scss';
+.button-submit{
+  background-color:$primary-color-0;
+  text-transform: uppercase;
+  border-radius: 15px;
+  width: 95%;
+  &:active{
+    border:1px solid $secondary-color-7;
+  }
+  &:hover{
+    background-color: #33266c;
+  }
+}
+
+
+</style>
